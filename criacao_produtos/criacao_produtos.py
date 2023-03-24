745372d434e0b6b2c05d02b02b148524be766bc1
joaovictor1516 <voliveira.joao30@sempreceub.com>
2023-03-24 10:25:26 -0300

    adicionando script da funcionalidade de adicionar produto

--- criacao_produtos/criacao_produtos.py
+++ criacao_produtos/criacao_produtos.py
@@ -5,3 +5,27 @@ import mysql.connector
 connection = mysql.connector.connect(user="user", database="catalago_produtos")
 
 app = FastAPI()
+
+class Product(BaseModel):
+    id: int | None
+    name: str
+    description: str
+    category: str
+    price: float
+
+@app.post('/creat_product')
+async def post_prosuct(product: Product):
+    cursor = connection.cursor(dictionary=True)
+    
+    stantment = 'insert into `products` (`product_name`, `product_description`,`product_category`,`product_price`)'\
+    'values (%s, %s, %s, %s)'
+
+    values = (product.name, product.description, product.category, product.price)
+
+    cursor.execute(stantment, values)
+
+    product.id = cursor.lastrowid
+
+    connection.commit()
+
+    return product
