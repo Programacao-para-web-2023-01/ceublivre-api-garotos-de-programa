2906e80130120b5185a7d15587bf783b38687dbf
João Victor Campos Souza Oliveira <voliveira.joao30@dinfor.uniceub>
2023-03-30 19:43:08 -0300

    insercao de imagem na criacao do produto

--- criacao_produtos/criacao_produtos.py
+++ criacao_produtos/criacao_produtos.py
@@ -2,6 +2,33 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import mysql.connector
 
-connection = mysql.connector.connect(user="user", database="catalago_produtos")
+connection = mysql.connector.connect(user="user", database="product_List", password="123456")
 
 app = FastAPI()
+
+
+class Product(BaseModel):
+    id: int | None
+    name: str
+    description: str
+    category: str
+    price: float
+    image: str
+
+
+@app.post('/creat_product')
+async def post_prosuct(product: Product):
+    cursor = connection.cursor(dictionary=True)
+
+    stantment = 'insert into `products` (`product_name`, `product_description`,`product_category`,`product_price`, product_image)' \
+                'values (%s, %s, %s, %s, %s)'
+
+    values = (product.name, product.description, product.category, product.price, product.image)
+
+    cursor.execute(stantment, values)
+
+    product.id = cursor.lastrowid
+
+    connection.commit()
+
+    return product
