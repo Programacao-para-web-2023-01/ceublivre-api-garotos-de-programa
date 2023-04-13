f95f4dd42610a2b06ef27ef1d52ca88ead033fba
João Paulo da Silveira <37747163+jaumsilveira@users.noreply.github.com>
2023-04-13 15:19:09 -0300

    Get ativos e Get by key (id)

--- listar_produtos/listar_produtos.py
+++ listar_produtos/listar_produtos.py
@@ -18,6 +18,8 @@ class Product(BaseModel):
     version: int
     active: int
 
+### Todos produtos cadastrados        
+        
 @app.get("/product")
 async def list_products():
     res = db.fetch()
@@ -30,3 +32,31 @@ async def list_products():
 
     return res.items
 
+### Produtos ativos
+
+
+@app.get("/product/active")
+async def get_active_products():
+    res = db.fetch({"active": 1})
+    all_items = res.items
+
+    # fetch until last is 'None'
+    while res.last:
+        res = db.fetch(last=res.last)
+        all_items += res.items
+
+    return all_items
+
+
+### Produto por key
+
+
+@app.get("/product/{key}")
+async def get_products_by_id(key: str):
+    product = db.get(id)
+    if product:
+        return product
+
+    raise HTTPException(status_code=404, detail="product not found")
+    
+    
