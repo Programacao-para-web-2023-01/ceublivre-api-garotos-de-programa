7c60aeb723b97c1f724c5d87133008ba450da4b7
joaovictor1516 <voliveira.joao30@sempreceub.com>
2023-04-13 12:03:33 -0300

    atualizando o controle_versao_produtos.py

--- controle_versao_produtos/controle_versao_produtos.py
+++ controle_versao_produtos/controle_versao_produtos.py
@@ -1,6 +1,32 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-import mysql.connector
+from deta import Deta
 
-connection = mysql.connector.connect(user='root', database='product_List', password='123456')
+deta = Deta("e0h2cutqoow_Qgi1mF4jpgxHGhDsS3mNj8MWttvPwiUa")
 
+app = FastAPI
+
+db = deta.base('Products')
+
+class Product(BaseModel):
+    key: str | None
+    name: str
+    description: str
+    price: float
+    image: str
+    version: int
+    active: int
+
+@app.put('/update_product/{key}')
+
+async def constrol_update_product(product: Product, key: str):
+    update = db.update(product.dict(exclude={'key'}), key)
+    
+    if  update != None:
+        raise HTTPException(status_code=404, detail="Product not found")
+    
+    return product
+#pensar em onde adicionar as versoes anteriores do produto
+# @app.post('/produto_destualizado/{key}')
+
+# async def 
