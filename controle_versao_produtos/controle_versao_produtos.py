0e8f9376ad5a777bc304fc9897c7367930c155d8
joaovictor1516 <voliveira.joao30@sempreceub.com>
2023-04-20 14:58:27 -0300

    adicao das tabela dos produtos destualizados e criacao da funcao que adiciona o produto desatualizado nesta tabela

--- controle_versao_produtos/controle_versao_produtos.py
+++ controle_versao_produtos/controle_versao_produtos.py
@@ -6,7 +6,9 @@ deta = Deta("e0h2cutqoow_Qgi1mF4jpgxHGhDsS3mNj8MWttvPwiUa")
 
 app = FastAPI
 
-db = deta.base('Products')
+db = deta.base('ProductDatabase')
+
+db_prod_outdated = deta.base('OutdatedProducts')
 
 class Product(BaseModel):
     key: str | None
@@ -17,6 +19,14 @@ class Product(BaseModel):
     version: int
     active: int
 
+#pensar em onde adicionar as versoes anteriores do produto
+@app.post('/outdated_products')
+
+async def outdated_products(product: Product):
+    create = db_prod_outdated.insert(product.dict(exclude={'key'}))
+
+    return create
+
 @app.put('/update_product/{key}')
 
 async def constrol_update_product(product: Product, key: str):
@@ -25,8 +35,5 @@ async def constrol_update_product(product: Product, key: str):
     if  update != None:
         raise HTTPException(status_code=404, detail="Product not found")
     
+    outdated_products(product)
     return product
-#pensar em onde adicionar as versoes anteriores do produto
-# @app.post('/produto_destualizado/{key}')
-
-# async def 
