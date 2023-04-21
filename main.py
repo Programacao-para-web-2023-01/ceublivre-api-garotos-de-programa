3106c333fdea4555a4cb613516a1d5e101b17db3
Paulo Lima <henriquecastro.paulo@sempreceub.com>
2023-04-20 21:08:45 -0300

    Atualizando main.py

--- main.py
+++ main.py
@@ -1,52 +1,74 @@
-from fastapi import FastAPI, HTTPException
+from typing import Annotated, Union
+from fastapi import FastAPI, HTTPException, File, UploadFile
 from pydantic import BaseModel
-import mysql.connector
-from dotenv import load_dotenv
-load_dotenv()
-import os
-import MySQLdb
+from deta import Deta
 
-connection = MySQLdb.connect(
-  host= os.getenv("HOST"),
-  user=os.getenv("USERNAME"),
-  passwd= os.getenv("PASSWORD"),
-  db= os.getenv("DATABASE"),
-  ssl_mode = "VERIFY_IDENTITY",
-  ssl      = {
-    "ca": "./etc/ssl/ca.pem"
-  }
-  
-)
 
-##connection = mysql.connector.connect(user="root", database="classroom", password = "123456")
 app = FastAPI()
 
-"""
+deta = Deta("e0h2cutqoow_Qgi1mF4jpgxHGhDsS3mNj8MWttvPwiUa")
+db = deta.Base("Products") #Base de produtos(ativos)
+drive = deta.Drive("Images") # Drive de Imagens (ativos)
+
 class Product(BaseModel):
-    id: int | None
+    key: str | None
     name: str
     description: str
     category: str
     price: float
-"""
-@app.get("/products")
+    image: str
+    version: int
+    active: int
+
+### Todos produtos cadastrados        
+@app.get("/product")
 async def list_products():
-    return "Hello World"
+    res = db.fetch()
+    all_items = res.items
+# fetch until last is 'None'
+    while res.last:
+        res = db.fetch(last=res.last)
+        all_items += res.items
+    return res.items
 
-"""
-@app.put("/product/{id}")
-async def update_product(id: int, product: Product):
-    cursor = connection.cursor(dictionary=True)
-    statement = "UPDATE `products` SET product_name = %s, product_description = %s, product_category = %s, product_price = %s "\
-                    "WHERE product_id = %s"
+### Produtos ativos
+@app.get("/product/active")
+async def get_active_products():
+    res = db.fetch({"active": 1})
+    all_items = res.items
+    # fetch until last is 'None'
+    while res.last:
+        res = db.fetch(last=res.last)
+        all_items += res.items
+    return all_items
 
-    value = (product.name, product.description, product.category, product.price, product.id)
+### Produto por key
+@app.get("/product/{key}")
+async def get_products_by_id(key: str):
+    product = db.get(id)
+    if product:
+        return product
+    raise HTTPException(status_code=404, detail="product not found")
 
-    cursor.execute(statement, value)
-    connection.commit()
+### Novo produto
+@app.post('/product')
+async def post_product(product: Product):
+    
+    inserted = db.insert(product.dict(exclude={'key'}))
+    return inserted
 
-    if cursor.rowcount == 0:
+### Inserção de Imagens
+### Ainda necessário integrar o JSON do registro de produto
+@app.put('/product')
+async def insert_image(file: Union[UploadFile, None] = None):
+    if not file:
+
+
+### Alterar produto 
+@app.put("/product/{key}")
+async def update_product(product: Product, key: str):
+    
+    updated = db.update(product.dict(exclude={'key'}), key)
+    if updated != None:
         raise HTTPException(status_code=404, detail="Product not found")
-
     return product
-"""
