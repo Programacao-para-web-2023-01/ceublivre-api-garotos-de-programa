0b2232dc97c05956dda5a0815da8299f290ea026
joaovictor1516 <voliveira.joao30@sempreceub.com>
2023-05-04 13:46:59 -0300

    criacao da funcionalidade de criar uma variacao do produto, ainda contem erros, este commit serve apenas para atualizar o arquivo

--- controle_versao_produtos/controle_versao_produtos.py
+++ controle_versao_produtos/controle_versao_produtos.py
@@ -6,13 +6,20 @@ deta = Deta("e0h2cutqoow_Qgi1mF4jpgxHGhDsS3mNj8MWttvPwiUa")
 
 app = FastAPI()
 
-db = deta.Base('Product')
+db = deta.Base('Products')
 
 db_prod_outdated = deta.Base('ProductDatabase')
 
+class BaseProduct(BaseModel):
+    name: str
+    description: str
+    price: float
+    image: str
+    weight: float
+
 class Product(BaseModel):
     key: str | None
-    first_key: str
+    first_key: str | None
     name: str
     description: str
     price: float
@@ -21,27 +28,41 @@ class Product(BaseModel):
     active: int
     weight: float
 
-#pensar em onde adicionar as versoes anteriores do produto
 @app.post('/outdated_Products')
-async def outdated_products(product: Product):
+async def outdated_products(product: Product, first_key: str):
     create = db_prod_outdated.insert(product.dict(exclude={'key'}))
 
     return create
 
 @app.put('/update_product/{key}')
-async def constrol_update_product(product: Product, key: str):
+async def constrol_update_product(product: Product, key: str, first_key: str): 
     update = db.update(product.dict(exclude={'key'}), key)
-    
+
     if  update != None:
         raise HTTPException(status_code=404, detail="Product not found")
     
-    outdated_products(product)
+    outdated_products(product, first_key)
     return product
 
-
-@app.post('/product_variation')
-
-async def product_variation(product: Product):
-    create = db.insert(product.dict(exclude={'key'}))
+@app.post("/creat_product_version/{first_key}")#usa a first key do produto que sera alterado
+async def product_variation(first_key: str, value: str):#o element_changed é o elemento que será alterado.o value e o valor novo valor do campo alterado, estes valores serao envidos pelo front-end
     
-    return create
+    new_variation: Product
+    new_variation = get_products_by_first_key(first_key)
+
+    new_variation.name = value
+
+    inserted = db.put(new_variation.dict)
+    new_variation.key = inserted.key
+    inserted = db.update(new_variation.dict, new_variation.key) 
+#     resolver o erro: new_variation.name = value
+#                      AttributeError: 'coroutine' object has no attribute 'name'
+
+    return inserted
+
+async def get_products_by_first_key(first_key: str):
+    product = db.get(first_key)
+    if product:
+        return product
+    else: 
+        return print("product not found")
